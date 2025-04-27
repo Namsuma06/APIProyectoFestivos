f8418707537d06e099878e7c429d511c7313c253
Namsuma06 <maria.benitez753@pascualbravo.edu.co>
2025-04-26 21:52:52 -0500

    actualizacion capa servicio

---
+++ src/app/tipo.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TiposService {
+  private baseUrl = 'https://localhost:7238/api/tipos';
+
+  constructor(private http: HttpClient) {}
+
+  listarTodos(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+  obtenerTipo(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/obtener/${id}`);
+  }
+
+  agregarTipo(tipo: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/agregar`, tipo);
+  }
+
+  modificarTipo(id: number, tipo: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/modificar/${id}`, tipo);
+  }
+
+  eliminarTipo(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/eliminar/${id}`);
+  }
+}
